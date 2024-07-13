f233931a3cc0fee53abe69894adb80c971abfcf9
kostiukevechdima <kostiukevechdima@gmail.com>
2024-07-13 23:13:07 +0300

    final commit

---
+++ src/assets/App/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { ContactList } from "../components/ContactList/ContactList.jsx";
+import { SearchBox } from "../components/SearchBox/SearchBox.jsx";
+import { ContactForm } from "../components/ContactForm/ContactForm.jsx";
+
+import './App.css';
+
+export default function App() {
+  const dataUserContact = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
+  const [contacts, setContacts] = useState(() => {
+    const savedContact = localStorage.getItem("my-key");
+    return savedContact !== null ? JSON.parse(savedContact) : dataUserContact;
+  });
+
+  const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  function filtered(e) {
+    setFilter(e.target.value);
+  }
+
+  const filterContact = () => {
+    return contacts.filter((user) =>
+      user.name.toLowerCase().trim().includes(filter.toLowerCase().trim())
+    );
+  };
+
+  const filteredContact = filterContact();
+
+  useEffect(() => {
+    window.localStorage.setItem("my-key", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = (userId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((user) => {
+        return user.id !== userId;
+      })
+    );
+  };
+
+  return (
+    <div className="mainContainer">
+      <h1 className="title">Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox onSearch={filtered} value={filter} />
+      <ContactList contactsData={filteredContact} onDelete={deleteContact} />
+    </div>
+  );
+}
